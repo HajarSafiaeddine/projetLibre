3caea65368ec6cb1725c4454c9e4f10d433144fe
HajarSafiaeddine <lhajar489@gmail.com>
2022-08-22 10:04:16 +0100

    update modifs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './Components/Navbar/Navbar'
 import Footer from './Components/Footer/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import Bitcoin from './Components/Bitcoin/Bitcoin';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
    <Navbar />
    <Routes>
    <Route path="/Home" element={<Home />} />
+   <Route path="/Bitcoin" element={<Home />} />
    <Route path="/*" element={<Home />} />
    </Routes>
    <Footer/>
